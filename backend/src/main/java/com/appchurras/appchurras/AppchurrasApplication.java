637b2b76ee38a4c4ec0fa9df028c634e0270be0a
andrebr2 <andrebrandao12@yahoo.com>
2022-08-02 15:15:56 -0300

    Backend project created

---
+++ backend/src/main/java/com/appchurras/appchurras/AppchurrasApplication.java
@@ -0,0 +1,13 @@
+package com.appchurras.appchurras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppchurrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppchurrasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/appchurras/appchurras/AppchurrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appchurras.appchurras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppchurrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

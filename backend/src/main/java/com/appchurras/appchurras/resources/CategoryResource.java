a61864759ee64b8bf1cc17b407cba7c611aca5e4
andrebr2 <andrebrandao12@yahoo.com>
2022-08-02 21:23:59 -0300

    DTO

--- backend/src/main/java/com/appchurras/appchurras/resources/CategoryResource.java
+++ backend/src/main/java/com/appchurras/appchurras/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.appchurras.appchurras.entities.Category;
+import com.appchurras.appchurras.dto.CategoryDTO;
 import com.appchurras.appchurras.services.CategoryService;
 
 @RestController
@@ -19,9 +19,9 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		
-		List<Category> list = service.findAll();
+		List<CategoryDTO> list = service.findAll();
 		
 
 		
--- backend/src/main/java/com/appchurras/appchurras/services/CategoryService.java
+++ backend/src/main/java/com/appchurras/appchurras/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.appchurras.appchurras.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.appchurras.appchurras.dto.CategoryDTO;
 import com.appchurras.appchurras.entities.Category;
 import com.appchurras.appchurras.repositories.CategoryRepository;
 
@@ -16,8 +18,12 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true) 
-	public List<Category> findAll() {
+	public List<CategoryDTO> findAll() {
 		
-		return repository.findAll();
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+	
 	}
 }

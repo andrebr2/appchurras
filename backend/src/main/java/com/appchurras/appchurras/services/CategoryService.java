c6727df4defed42701978d6b3a2215dc25ad5a65
andrebr2 <andrebrandao12@yahoo.com>
2022-08-03 00:08:02 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/appchurras/appchurras/services/CategoryService.java
+++ backend/src/main/java/com/appchurras/appchurras/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.appchurras.appchurras.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.appchurras.appchurras.dto.CategoryDTO;
 import com.appchurras.appchurras.entities.Category;
 import com.appchurras.appchurras.repositories.CategoryRepository;
+import com.appchurras.appchurras.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -25,5 +27,14 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
 	
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found."));
+		return new CategoryDTO(entity);
+		
 	}
 }
